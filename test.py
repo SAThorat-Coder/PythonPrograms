ed3d106e72d1fc921837992b4b6521c38f9e3aa6
SAThorat-Coder <thoratsaurabh0105@gmail.com>
2026-04-05 23:39:12 +0530

    Modify print statements and add comments in test.py
    Updated print statements and added comments for clarity.

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-print("whats up Buddy..! V6")
+#This is First line for modified V6 to V7!
+print("whats up Buddy..! V7!")
+#This is second modified line for testing
+print("This is for jenkins testing..!")
+
