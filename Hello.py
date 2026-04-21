5f5b5659c686a20f118ee8a84b0a7ea8bfd5d531
SAThorat-Coder <thoratsaurabh0105@gmail.com>
2026-04-21 14:10:05 +0530

    Add print statement for email notification test
    Add email notification test print statement

--- Hello.py
+++ Hello.py
@@ -3,3 +3,5 @@ print("This is my first Program in Python....!")
 # Need add verison
 print("This is the V1 code for my app")
 print("Add Line for testing purpose")
+#This will test the my email.notification is working or not 
+print("This is will test Eamil Notification")
